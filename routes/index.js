0ce26663e5bfe484320d29ece66f9de79aa0f6f2
yangttlxl@sina.com <yangttlxl@sina.com>
2017-03-23 17:36:12 +0800

    增加新的package

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
+var wechat = require('wechat');
+var API = require('wechat-api');
+
+var config = {
+  token: 'weixin',
+  appid: 'wx30b4d102680fa0c2',
+  appsecret:'923dff6aae0d46e06786a306a476dc51',
+  encodingAESKey: 'COlVMNkJocnZRQ64AszKHuyLfS7M7wxI6nRh2Iz19GI',
+  checkSignature: true // 可选，默认为true。由于微信公众平台接口调试工具在明文模式下不发送签名，所以如要使用该测试工具，请将其设置为false
+};
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/test',  wechat(config,function(req, res, next) {}));
 
 module.exports = router;
