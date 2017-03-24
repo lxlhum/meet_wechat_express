01746c127eca52d6e4e70c76dbd961b3035d6ac7
yangttlxl@sina.com <yangttlxl@sina.com>
2017-03-24 10:11:18 +0800

    增加appid

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,10 @@ var wechat = require('wechat');
 var API = require('wechat-api');
 
 var config = {
-  token: 'weixin',
-  appid: 'wx30b4d102680fa0c2',
-  appsecret:'923dff6aae0d46e06786a306a476dc51',
-  encodingAESKey: 'COlVMNkJocnZRQ64AszKHuyLfS7M7wxI6nRh2Iz19GI',
+  token: 'meet',
+  appid: 'wx3a3f138d198893c4',
+  appsecret:'95bdcfaa2bd98f2b9c1bf294570252bd',
+  encodingAESKey: 'AjF2DgN0oCc7aq9sWXGvo8VtKXoWfQREoMOO6eVxfDc',
   checkSignature: true // 可选，默认为true。由于微信公众平台接口调试工具在明文模式下不发送签名，所以如要使用该测试工具，请将其设置为false
 };
 
